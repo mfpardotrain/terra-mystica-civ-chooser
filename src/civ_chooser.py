db05e71142b1574d1bfb3e749716ad7e726b07d3
mfpardotrain <pardotrain@gmail.com>
2020-10-13 11:53:03 -0500

    Fix button size

--- src/civ_chooser.py
+++ src/civ_chooser.py
@@ -6,7 +6,7 @@ class App:
     def __init__(self, win, n_choices):
         self.win = win
         self.win.title("Terra Mystica Civ Chooser")
-        self.win.geometry("305x305")
+        self.win.geometry("400x400")
         tk.Label(self.win, text="Choose a Civ:").pack()
 
         self.chosen = []
@@ -28,29 +28,26 @@ class App:
         tk.Button(undo_frame, text="Refresh Choices", command=self.assign_picks).grid(row=7, column=5, pady=10, padx=5)
 
     def assign_picks(self, undo=False):
-        if undo:
-            self.render_button_choices()
-        else:
-            colors = ["Yellow", "Yellow", "Brown", "Brown", "Black", "Black", "Blue", "Blue", "Grey", "Grey", "Red", "Red"]
-            civs = ["Fakirs", "Nomads", "Halflings", "Cultists", "Darklings", "Alchemists", "Mermaids", "Swarmlings",
-                    "Engineers", "Dwarves", "Chaos Magicians", "Giants"]
+        colors = ["Yellow", "Yellow", "Brown", "Brown", "Black", "Black", "Blue", "Blue", "Grey", "Grey", "Red", "Red"]
+        civs = ["Fakirs", "Nomads", "Halflings", "Cultists", "Darklings", "Alchemists", "Mermaids", "Swarmlings",
+                "Engineers", "Dwarves", "Chaos Magicians", "Giants"]
 
-            indices = [civs.index(civ) for civ in self.chosen]
-            [indices.append(index + 1) for index in indices if index % 2 == 0]
+        indices = [civs.index(civ) for civ in self.chosen]
+        [indices.append(index + 1) for index in indices if index % 2 == 0]
 
-            available_civs = [i for j, i in enumerate(civs) if j not in indices]
-            available_colors = [i for j, i in enumerate(colors) if j not in indices]
+        available_civs = [i for j, i in enumerate(civs) if j not in indices]
+        available_colors = [i for j, i in enumerate(colors) if j not in indices]
 
-            randomized = random.sample(range(0, len(available_civs)), len(available_civs))
-            randomized_civs = [available_civs[i] for i in randomized]
-            randomized_colors = [available_colors[i] for i in randomized]
+        randomized = random.sample(range(0, len(available_civs)), len(available_civs))
+        randomized_civs = [available_civs[i] for i in randomized]
+        randomized_colors = [available_colors[i] for i in randomized]
 
-            civ_dict = dict(zip(randomized_colors, randomized_civs))
+        civ_dict = dict(zip(randomized_colors, randomized_civs))
 
-            sample = random.sample(civ_dict.keys(), self.n_choices)
+        sample = random.sample(civ_dict.keys(), self.n_choices)
 
-            self.chosen_three = [civ_dict[color] for color in sample]
-            self.render_button_choices()
+        self.chosen_three = [civ_dict[color] for color in sample]
+        self.render_button_choices()
 
     def render_button_choices(self):
         button_frame = tk.Frame(self.win)
@@ -58,7 +55,7 @@ class App:
 
         for i in range(self.n_choices):
             self.button_list.append(
-                tk.Button(button_frame, text=self.chosen_three[i], command=lambda c=i: self.command(c)))
+                tk.Button(button_frame, text=self.chosen_three[i], command=lambda c=i: self.command(c), width=14))
             self.button_list[i].config(text=self.chosen_three[i])
             self.button_list[i].grid(row=0, column=i, padx=10, pady=5)
 
@@ -72,7 +69,7 @@ class App:
             print("Error: cannot have more than 5 civs in a game.")
 
     def undo(self):
-        if (len(self.chosen) > 0):
+        if len(self.chosen) > 0:
             self.chosen.pop()
             self.create_chosen_string()
 
@@ -84,7 +81,7 @@ class App:
 
     def create_chosen_string(self):
         self.chosen_string = f"{', '.join(self.chosen)}"
-        chosen_label = tk.Label(self.chosen_frame, name="chosen_string", text=f"Chosen: {self.chosen_string}", wraplength=200)
+        chosen_label = tk.Label(self.chosen_frame, name="chosen_string", text=f"Chosen: {self.chosen_string}", wraplength=300)
         chosen_label.grid(row=6, column=0, padx=5)
 
 
