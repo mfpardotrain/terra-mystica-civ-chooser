52ace1ce3e72a2b04042a664def40addd6dc6254
mfpardotrain <pardotrain@gmail.com>
2020-10-15 09:49:21 -0500

    Object orient this

--- src/civ_chooser.py
+++ src/civ_chooser.py
@@ -2,22 +2,30 @@ import random
 import tkinter as tk
 import os
 import sys
+from src.faction import Faction
 
 
 class App:
     def __init__(self, win, n_choices):
         os.chdir(os.path.dirname(sys.argv[0]))
-        print(os.getcwd())
         self.win = win
         self.win.title("Terra Mystica Civ Chooser")
         self.win.geometry("800x500")
         tk.Label(self.win, text="Choose a Civ:").pack()
 
-        self.chosen = []
         self.n_choices = n_choices
         self.button_list = []
-        self.chosen_three = []
+        self.to_choose = []
         self.previous_choices = []
+
+        colors = ["Yellow", "Yellow", "Brown", "Brown", "Black", "Black", "Blue", "Blue", "Grey", "Grey", "Red", "Red",
+                  "Green", "Green"]
+        civs = ["Fakirs", "Nomads", "Halflings", "Cultists", "Darklings", "Alchemists", "Mermaids", "Swarmlings",
+                "Engineers", "Dwarves", "Chaos_Magicians", "Giants", "Witches", "Auren"]
+        self.factions = []
+        for i in range(0, len(colors)):
+            self.factions.append(Faction(color=colors[i], name=civs[i], image=f"images/{civs[i]}.gif", index=i,
+                                 is_chosen=False, is_available=True))
 
         self.assign_picks()
 
@@ -33,30 +41,9 @@ class App:
         tk.Button(undo_frame, text="Refresh Choices", command=self.assign_picks).grid(row=7, column=5, pady=10, padx=5)
 
     def assign_picks(self):
-        colors = ["Yellow", "Yellow", "Brown", "Brown", "Black", "Black", "Blue", "Blue", "Grey", "Grey", "Red", "Red", "Green", "Green"]
-        civs = ["Fakirs", "Nomads", "Halflings", "Cultists", "Darklings", "Alchemists", "Mermaids", "Swarmlings",
-                "Engineers", "Dwarves", "Chaos_Magicians", "Giants", "Witches", "Auren"]
-
-        indices = [civs.index(civ) for civ in self.chosen]
-
-        for civ in self.chosen:
-            if civs.index(civ) % 2 == 0:
-                indices.append(civs.index(civ) + 1)
-            elif civs.index(civ) % 2 == 1:
-                indices.append(civs.index(civ) - 1)
-
-        available_civs = [i for j, i in enumerate(civs) if j not in indices]
-        available_colors = [i for j, i in enumerate(colors) if j not in indices]
-
-        randomized = random.sample(range(0, len(available_civs)), len(available_civs))
-        randomized_civs = [available_civs[i] for i in randomized]
-        randomized_colors = [available_colors[i] for i in randomized]
-
-        civ_dict = dict(zip(randomized_colors, randomized_civs))
-
-        sample = random.sample(civ_dict.keys(), self.n_choices)
-
-        self.chosen_three = [civ_dict[color] for color in sample]
+        available_civs = [faction for faction in self.factions if faction.is_available]
+        sample = random.sample(available_civs, self.n_choices)
+        self.to_choose = [faction for faction in sample]
         self.render_button_choices()
 
     def render_button_choices(self):
@@ -64,47 +51,58 @@ class App:
         button_frame.pack()
 
         for i in range(self.n_choices):
-            chosen = self.chosen_three[i]
-            self.button_list.append(
-                tk.Button(button_frame, text=chosen.replace("_", " "), command=lambda c=i: self.command(c), width=14))
-            self.button_list[i].config(text=chosen.replace("_", " "))
-            self.button_list[i].bind("<Enter>", lambda event, name=chosen: self.on_enter(e=event, name=name))
+            chosen_name = self.to_choose[i].name
+            faction = self.to_choose[i]
+            self.button_list.append(tk.Button(button_frame, text=chosen_name.replace("_", " "),
+                                              command=lambda c=i: self.command(c), width=14))
+            self.button_list[i].config(text=chosen_name.replace("_", " "))
+            self.button_list[i].bind("<Enter>", lambda event, f=faction: self.on_enter(e=event, f=f))
             self.button_list[i].bind("<Leave>", self.on_leave)
             self.button_list[i].grid(row=0, column=i, padx=10, pady=5)
 
     def command(self, c):
-        if len(self.chosen) < 5:
+        f = self.to_choose[c]
+        n_chosen = len([faction for faction in self.factions if faction.is_chosen])
+        other_color = [faction for faction in self.factions if faction.color == f.color][0]
+        if n_chosen < 5:
             self.background_label.configure(image=None)
             self.background_label.photo = None
             self.background_label.pack()
 
-            self.chosen.append(self.chosen_three[c])
-            self.previous_choices.append(self.chosen_three)
+            f.is_chosen = True
+            f.is_available = False
+            other_color.is_available = False
+
+            self.previous_choices.append(self.to_choose)
             self.create_chosen_string()
-            if len(self.chosen) < 4:
+            if n_chosen < 4:
                 self.assign_picks()
         else:
             print("Error: cannot have more than 5 civs in a game.")
 
     def undo(self):
-        if len(self.chosen) > 0:
-            self.chosen.pop()
+        n_chosen = len([faction for faction in self.factions if faction.is_chosen])
+        last_choices = self.previous_choices[len(self.previous_choices) - 1]
+        if n_chosen > 0:
+            previous_choice = [faction for faction in last_choices if faction.is_chosen][0]
+            previous_choice.is_chosen = False
             self.create_chosen_string()
 
-            self.chosen_three = self.previous_choices[len(self.previous_choices) - 1]
+            self.to_choose = last_choices
             self.previous_choices.pop()
             self.render_button_choices()
         else:
             print("Error: none chosen")
 
     def create_chosen_string(self):
-        self.chosen_string = f"{', '.join(self.chosen)}"
+        chosen_names = [faction.name for faction in self.factions if faction.is_chosen]
+        self.chosen_string = f"{', '.join(chosen_names)}"
         text = f"Chosen: {self.chosen_string}".replace("_", " ")
         chosen_label = tk.Label(self.chosen_frame, name="chosen_string", text=text, wraplength=200)
         chosen_label.grid(row=6, column=0, padx=5)
 
-    def on_enter(self, name, e):
-        background_image = tk.PhotoImage(file=f"images/{name}.gif")
+    def on_enter(self, f, e):
+        background_image = tk.PhotoImage(file=f.image)
         self.background_label.configure(image=background_image)
         self.background_label.photo = background_image
         self.background_label.pack()
---
+++ src/faction.py
@@ -0,0 +1,9 @@
+
+class Faction:
+    def __init__(self, color, name, image, index, is_chosen, is_available):
+        self.index = index
+        self.color = color
+        self.name = name
+        self.image = image
+        self.is_chosen = is_chosen
+        self.is_available = is_available
